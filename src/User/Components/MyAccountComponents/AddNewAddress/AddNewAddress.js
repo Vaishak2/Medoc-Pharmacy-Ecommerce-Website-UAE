a9df72f4981013090a3f3b6d3f3e3536bc96c47c
Ruthe46 <ruthinlosh.web.solwyz@gmail.com>
2024-08-07 15:02:42 +0530

    Add new address added

---
+++ src/User/Components/MyAccountComponents/AddNewAddress/AddNewAddress.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function AddNewAddress() {
+  return (
+    <div>
+      <div className='sm:text-[16px] sm:leading-6 font-semibold text-left'>Add New Address</div>
+      <div className='bg-[#304BA0] sm:mt-8 py-[14px] px-6 w-fit hover:bg-[#3E5FC5] rounded-lg'>
+        <div></div>
+        <div className='text-[14px] text-[#FFFFFF] tracking-wide'>Use my current location</div>
+      </div>
+    </div>
+  )
+}
+
+export default AddNewAddress
