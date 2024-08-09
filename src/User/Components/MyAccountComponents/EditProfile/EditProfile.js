aacd2331586c555917cfdd9a0580b6102d4bb790
Ruthe46 <ruthinlosh.web.solwyz@gmail.com>
2024-08-09 09:50:53 +0530

    Edit Profile Structure added

---
+++ src/User/Components/MyAccountComponents/EditProfile/EditProfile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EditProfile() {
+  return (
+    <div>
+      <div className='sm:text-[16px] sm:leading-6 font-semibold text-left'>Edit Profile</div>
+    </div>
+  )
+}
+
+export default EditProfile
