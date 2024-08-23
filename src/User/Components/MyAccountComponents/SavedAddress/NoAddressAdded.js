884e0c6a57b791c7966e53645c2b0bf395b32c9e
Vineesh <vineesh.web.solwyz@gmail.com>
2024-08-23 14:05:49 +0530

    commit

--- src/User/Components/MyAccountComponents/SavedAddress/NoAddressAdded.js
+++ src/User/Components/MyAccountComponents/SavedAddress/NoAddressAdded.js
@@ -10,11 +10,11 @@ function NoAddressAdded() {
         <div className='sm:w-16 sm:h-16 bg-[#EDF1FF] rounded-full py-4 mx-auto'>
   <img className='justify-center m-auto' src={NoAddress} alt="" />
         </div>
-        <div className='sm:text-[18px] sm:font-medium sm:leading-[20px]'>
+        <div className='sm:text-[18px] sm:font-medium sm:leading-[20px] mt-6'>
         No addresses added
         </div>
         <div className='text-[#A8A8A8] sm:text-sm sm:font-normal mt-4'>There are no addresses added to this account. Please add an address</div>
-        <div className='sm:w-[396px] border border-[#304BA0]  rounded-lg text-[#304BA0] sm:font-medium px-8 py-5 sm:text-[14px] leading-4 mt-6 flex text-justify sm:ml- '>
+        <div className='sm:w-[396px] border border-[#304BA0] hover:bg-[#3E5FC5] rounded-lg text-[#304BA0] sm:font-medium px-8 py-5 sm:text-[14px] leading-4 mt-6 flex text-justify sm:ml- '>
        <div className='flex mx-auto'> <img className=' w-4 mr-2' src={AddCircle} alt="" />
         Add New Address
         </div>
